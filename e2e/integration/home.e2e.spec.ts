b17fd848dfb6fd3bbe3131b741e13039a612fd08
Saul <saul.moro.gomez@gmail.com>
2021-03-24 14:06:27 +0100

    test(e2e): add home e2e test

--- e2e/integration/home.e2e.spec.ts
+++ e2e/integration/home.e2e.spec.ts
@@ -1,9 +1,10 @@
 describe('Home', () => {
   before(() => {
     cy.visit('/');
+    cy.viewport(1251, 882);
   });
 
-  it('loads examples', () => {
-    cy.contains('Replace me with something relevant');
+  it('should display welcome msg', () => {
+    cy.findByText(/Start with Ionic/i);
   });
 });
