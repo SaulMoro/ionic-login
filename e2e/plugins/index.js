3f9a88b77567b2a644e9a45964be9db10eb84410
Saul <saul.moro.gomez@gmail.com>
2021-03-24 09:52:45 +0100

    test(e2e): add cypress support for e2e tests

---
+++ e2e/plugins/index.js
@@ -0,0 +1,11 @@
+const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
+
+/// <reference types="cypress" />
+
+module.exports = (on, config) => {
+  on('file:preprocessor', cypressTypeScriptPreprocessor);
+
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  return config;
+};
