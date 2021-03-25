58e05e2473cb56afde12621fb3d707bd77869d2f
Saul <saul.moro.gomez@gmail.com>
2021-03-25 12:57:26 +0100

    test(login): add firsts container tests with mock store

--- src/app/features/login/containers/login-container/login-container.component.spec.ts
+++ src/app/features/login/containers/login-container/login-container.component.spec.ts
@@ -1,24 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { render, screen } from '@testing-library/angular';
 
+import { selectError, selectLoading } from '@app/core/auth/+state/auth.selectors';
+import { SharedModule } from '@app/shared/shared.module';
 import { LoginContainerComponent } from './login-container.component';
 
 describe('LoginContainerComponent', () => {
-  let component: LoginContainerComponent;
-  let fixture: ComponentFixture<LoginContainerComponent>;
+  it('should create with Mock Store', async () => {
+    await setup();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoginContainerComponent],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(screen.getByRole('button', { name: /Acceder/i })).toBeTruthy();
   });
 });
+
+export const setup = async (loading = false, error: string | null = null) => {
+  const container = await render(LoginContainerComponent, {
+    imports: [SharedModule],
+    providers: [
+      provideMockStore({
+        selectors: [
+          {
+            selector: selectLoading,
+            value: loading,
+          },
+          {
+            selector: selectError,
+            value: error,
+          },
+        ],
+      }),
+    ],
+  });
+
+  const store = TestBed.inject(MockStore);
+  store.dispatch = jasmine.createSpy();
+
+  return { container, store };
+};
